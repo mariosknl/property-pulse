cce88ef5e421c7d25c5f48ed3446fe08f326260b
mariosknl <mariosknl@outlook.com>
2024-06-06 13:04:33 +0300

    create properties & cloudinary integration implemented

--- utils/request.js
+++ utils/request.js
@@ -2,44 +2,44 @@ const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 // Fetch all properties
 async function fetchProperties() {
-	try {
-		// handle the case where the domain is not available yet
-		if (!apiDomain) {
-			return [];
-		}
+  try {
+    // handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return [];
+    }
 
-		const res = await fetch(`${apiDomain}/properties`);
+    const res = await fetch(`${apiDomain}/properties`, { cache: 'no-store' });
 
-		if (!res.ok) {
-			throw new Error("Failed to fetch properties");
-		}
+    if (!res.ok) {
+      throw new Error("Failed to fetch properties");
+    }
 
-		return res.json();
-	} catch (error) {
-		console.log(error);
-		return [];
-	}
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 }
 
 // Fetch single property
 async function fetchProperty(id) {
-	try {
-		// handle the case where the domain is not available yet
-		if (!apiDomain) {
-			return null;
-		}
+  try {
+    // handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return null;
+    }
 
-		const res = await fetch(`${apiDomain}/properties/${id}`);
+    const res = await fetch(`${apiDomain}/properties/${id}`);
 
-		if (!res.ok) {
-			throw new Error("Failed to fetch properties");
-		}
+    if (!res.ok) {
+      throw new Error("Failed to fetch properties");
+    }
 
-		return res.json();
-	} catch (error) {
-		console.log(error);
-		return null;
-	}
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 }
 
 export { fetchProperties, fetchProperty };
